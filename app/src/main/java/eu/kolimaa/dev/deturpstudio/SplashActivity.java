c4699409f2bfa1386a2c4a20242c4d75b03f05d1
Artemy <lagunemine@gmail.com>
2015-02-26 01:16:19 +0300

    SplashActivity is ready

--- app/src/main/java/eu/kolimaa/dev/deturpstudio/SplashActivity.java
+++ app/src/main/java/eu/kolimaa/dev/deturpstudio/SplashActivity.java
@@ -5,6 +5,7 @@ import eu.kolimaa.dev.deturpstudio.util.SystemUiHider;
 import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,11 +15,23 @@ import android.view.View;
 
 public class SplashActivity extends Activity {
 
+    Intent init;
+    private static final int SPLASH_TIME = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                init = new Intent(SplashActivity.this, StartActivity.class);
+                startActivity(init);
+                finish();
+            }
+        }, SPLASH_TIME);
 
     }
 
