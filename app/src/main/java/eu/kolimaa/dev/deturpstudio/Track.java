40de84b4534c9c4e417a4f1d735b1ec9f01412a6
Artemy <lagunemine@gmail.com>
2015-03-04 11:13:04 +0300

    added the TracklistAdapter, Track stubs

--- app/src/main/java/eu/kolimaa/dev/deturpstudio/Track.java
+++ app/src/main/java/eu/kolimaa/dev/deturpstudio/Track.java
@@ -4,4 +4,9 @@ package eu.kolimaa.dev.deturpstudio;
  * Describes the Track field entities which should appear inside the StartActivity's Track mPlaylistView
  */
 public class Track {
+
+    public Track() {
+
+    }
+
 }
